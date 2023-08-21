d51e13b33b64fa6c7c652a1e727d36d77dab0033
Vitor Ferreira <vitorg_s@hotmail.com>
2023-08-20 22:52:53 -0300

    WIP

--- src/app/effects/user.effects.ts
+++ src/app/effects/user.effects.ts
@@ -1,26 +1,25 @@
-import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
+import { catchError, map, mergeMap } from 'rxjs/operators';
+
 import { EMPTY } from 'rxjs';
-import { map, mergeMap, catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
 import { UsersService } from '../services/users.service';
 
 @Injectable()
 export class UserEffects {
-
   @Effect()
-  loadUsers$ = this.actions$
-    .pipe(
-      ofType('[Movies Page] Load Movies'),
-      mergeMap(() => this.usersService.getAll()
-        .pipe(
-          map(users => ({ type: '[Movies API] Movies Loaded Success', payload: users })),
-          catchError(() => EMPTY)
-        ))
+  loadUsers$ = this.actions$.pipe(
+    ofType('[Movies Page] Load Movies'),
+    mergeMap(() =>
+      this.usersService.getAll().pipe(
+        map((users) => ({
+          type: '[Movies API] Movies Loaded Success',
+          payload: users,
+        })),
+        catchError(() => EMPTY)
       )
-    );
+    )
+  );
 
-  constructor(
-    private actions$: Actions,
-    private usersService: UsersService
-  ) {}
+  constructor(private actions$: Actions, private usersService: UsersService) {}
 }
